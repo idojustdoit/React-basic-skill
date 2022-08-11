cb0fd5b2d0e7931d1e7ed05df8274eff77d68dce
yulchan <yulchan9478@gmail.com>
2022-08-11 10:42:00 +0900

    [feat] webpack 추가 , 끝말잇기 추가

---
+++ webpack/client.jsx
@@ -0,0 +1,6 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+const WordRelayF = require("./WordRelayF");
+
+ReactDOM.render(<WordRelayF />, document.querySelector("#root"));
