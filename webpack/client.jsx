ae92922b41009e3943236cdabbc715e82664d7b5
yulchan <yulchan9478@gmail.com>
2022-08-11 10:47:18 +0900

    Revert "Revert "Revert "[feat] webpack 추가 , 끝말잇기 추가"""
    This reverts commit 49436de376ed4335c9a916423d088a1f28a22857.

--- webpack/client.jsx
+++
@@ -1,6 +0,0 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-
-const WordRelayF = require("./WordRelayF");
-
-ReactDOM.render(<WordRelayF />, document.querySelector("#root"));
